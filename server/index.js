f0978da65d34b470af4c4ce2ea032fa52e5379ca
ruta1402 <ruta.kulkarni@somaiya.edu>
2023-07-02 22:54:54 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -12,47 +12,47 @@ const SERVER_PORT = 9011;
 
 // Define a route to handle the socket connection and market data processing
 app.get('/market-data', (req, res) => {
-  try {
-    // Establish a socket connection to the server
-    const socket = new net.Socket();
-    socket.connect(SERVER_PORT, SERVER_HOST, () => {
-      console.log(`Connected to the server on port ${SERVER_PORT}`);
+    try {
+        // Establish a socket connection to the server
+        const socket = new net.Socket();
+        socket.connect(SERVER_PORT, SERVER_HOST, () => {
+            console.log(`Connected to the server on port ${SERVER_PORT}`);
 
-      // Send an initial request packet
-      socket.write(Buffer.from([1])); // Send a single byte as the initial request
-      console.log('Initial request sent');
-    });
+            // Send an initial request packet
+            socket.write(Buffer.from([1])); // Send a single byte as the initial request
+            console.log('Initial request sent');
+        });
 
-    // Receive and process market data packets
-    const buffer = Buffer.alloc(130); // market data packet size is 130 bytes
-    socket.on('data', (data) => {
-      // Read a market data packet
-      data.copy(buffer);
-      const bytesRead = data.length;
+        // Receive and process market data packets
+        const buffer = Buffer.alloc(130); // market data packet size is 130 bytes
+        socket.on('data', (data) => {
+            // Read a market data packet
+            data.copy(buffer);
+            const bytesRead = data.length;
 
-      // Process the market data packet
-      var fin =processMarketDataPacket(buffer, bytesRead);
-      // const jsonData = buffer.toString('utf8', 4, buffer.length);
-      // const parsedData = JSON.parse(jsonData);
-      // res.json({data:jsonData})
-      // console.log(jsonData);
-    });
+            // Process the market data packet
+            var fin = processMarketDataPacket(buffer, bytesRead);
+            // const jsonData = buffer.toString('utf8', 4, buffer.length);
+            // const parsedData = JSON.parse(jsonData);
+            // res.json({data:jsonData})
+            // console.log(jsonData);
+        });
 
-    // Handle the end of the socket connection
-    socket.on('end', () => {
-      console.log('Disconnected from the server');
-      res.send('Socket connection closed');
-    });
+        // Handle the end of the socket connection
+        socket.on('end', () => {
+            console.log('Disconnected from the server');
+            res.send('Socket connection closed');
+        });
 
-    // Handle socket errors
-    socket.on('end', () => {
-        console.log('Disconnected from the server');
-        res.send('Socket connection closed');
-      });
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Internal Server Error');
-  }
+        // Handle socket errors
+        socket.on('end', () => {
+            console.log('Disconnected from the server');
+            res.send('Socket connection closed');
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
 });
 
 app.listen(port, () => {
@@ -76,57 +76,50 @@ function processMarketDataPacket(packetData, packetSize) {
     const prevClosePrice = getLongFromLittleEndian(packetData, 114);
     const prevOpenInterest = getLongFromLittleEndian(packetData, 122);
     let option = '';
-    
+
     // Extract other fields as needed
     if (tradingSymbol.endsWith('CE')) {
-      option = 'Call';
+        option = 'Call';
     } else if (tradingSymbol.endsWith('PE')) {
-      option = 'Put';
+        option = 'Put';
     }
     //Index
-    var a="ALLBANKS";
-    var b="MAINIDX";
-    var c="FINANCIAL";
-    var d="MIDCAPS";
+    var a = "ALLBANKS";
+    var b = "MAINIDX";
+    var c = "FINANCIAL";
+    var d = "MIDCAPS";
 
-    var len_symbol=tradingSymbol.length;
+    var len_symbol = tradingSymbol.length;
 
-    var index="";
+    var index = "";
 
-    if(tradingSymbol.startsWith(a))
-    {   
-        index=a;
-    }
-    else if(tradingSymbol.startsWith(b))
-    {
-        index=b;
-    }
-    else if(tradingSymbol.startsWith(c))
-    {
-        index=c;
-    }
-    else if(tradingSymbol.startsWith(d))
-    {
-        index=d;   
+    if (tradingSymbol.startsWith(a)) {
+        index = a;
+    } else if (tradingSymbol.startsWith(b)) {
+        index = b;
+    } else if (tradingSymbol.startsWith(c)) {
+        index = c;
+    } else if (tradingSymbol.startsWith(d)) {
+        index = d;
     }
 
     //to check why array out of bound exception
 
     //expiry date
-    
-    var expiryDate=tradingSymbol.substring(index.length,index.length+7);
+
+    var expiryDate = tradingSymbol.substring(index.length, index.length + 7);
 
     //strike price
-    var strikePriceStr = tradingSymbol.slice(index.length+7,tradingSymbol.length).replace ( /[^0-9]/g, '' );
+    var strikePriceStr = tradingSymbol.slice(index.length + 7, tradingSymbol.length).replace(/[^0-9]/g, '');
     var strikePriceInt = parseInt(strikePriceStr);
-    
+
     // long strikePrice=Long.parseLong(strikePriceStr);
-    
+
     // Do further processing or display the extracted data
     console.log('Symbol:', tradingSymbol);
-    console.log("Index: ",index);
-    console.log("Expiry Date: ",expiryDate);
-    console.log("Strike Price: ",strikePriceInt);
+    console.log("Index: ", index);
+    console.log("Expiry Date: ", expiryDate);
+    console.log("Strike Price: ", strikePriceInt);
     console.log('Time Stamp:', timeStamp);
     console.log('Sequence:', sequenceNumber);
     console.log('Last Traded Price:', lastTradedPrice);
@@ -139,15 +132,15 @@ function processMarketDataPacket(packetData, packetSize) {
     console.log('Previous Close Price:', prevClosePrice);
     console.log('Previous Open Interest:', prevOpenInterest);
     console.log("");
-    
+
     // Implement the rest of the processing logic as needed
-  }
-  
-  function getLongFromLittleEndian(data, offset) {
+}
+
+function getLongFromLittleEndian(data, offset) {
     let value = 0;
     for (let i = 7; i >= 0; i--) {
-      value <<= 8;
-      value |= (data[offset + i] & 0xff);
+        value <<= 8;
+        value |= (data[offset + i] & 0xff);
     }
     return value;
-  }
+}
