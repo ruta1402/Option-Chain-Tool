79224dfd350d580cf2b85294e0abcc9cfaa7af3f
ParthChaudhary28 <parth.chaudhary@somaiya.edu>
2023-07-02 19:22:09 +0530

    new

--- readJarresp.java
+++ readJarresp.java
@@ -1,6 +1,9 @@
-import org.apache.commons.math3.distribution.NormalDistribution;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
 
-public class ReadJarResponse {
+public class readJarresp {
     private static final String SERVER_HOST = "localhost";
     private static final int SERVER_PORT = 9010;
 
@@ -84,4 +87,50 @@ public class ReadJarResponse {
         }
 
         // Calculate implied volatility (IV) using the Black Scholes formula
-        double underlyingPrice = lastTradedPrice
+        double underlyingPrice = lastTradedPrice / 100.0; // Convert to the appropriate unit
+        double strikePrice = ...; // Provide the actual strike price
+        double riskFreeRate = ...; // Provide the actual risk-free interest rate
+        double timeToMaturity = ...; // Provide the actual time to maturity
+        double impliedVolatility = calculateImpliedVolatility(underlyingPrice, strikePrice, riskFreeRate, timeToMaturity);
+
+        // Do further processing or display the calculated IV
+
+        System.out.println("Symbol: " + tradingSymbol);
+        System.out.println("Index: " + index);
+        System.out.println("Option: " + option);
+        System.out.println("Time Stamp: " + timeStamp);
+        System.out.println("Sequence: " + sequenceNumber);
+        System.out.println("Last Traded Price: " + lastTradedPrice);
+        System.out.println("Total Traded Volume: " + totalTradedVolume);
+        System.out.println("Best Bid: " + bestBid);
+        System.out.println("Best Ask: " + bestAsk);
+        System.out.println("Best Bid Quantity: " + bestBidQty);
+        System.out.println("Best Ask Quantity: " + bestAskQty);
+        System.out.println("Open Interest: " + openInterest);
+        System.out.println("Previous Close Price: " + prevClosePrice);
+        System.out.println("Previous Open Interest: " + prevOpenInterest);
+        System.out.println("Implied Volatility: " + impliedVolatility);
+        System.out.println();
+    }
+
+    private static long getLongFromLittleEndian(byte[] data, int offset) {
+        long value = 0;
+        for (int i = 7; i >= 0; i--) {
+            value <<= 8;
+            value |= (data[offset + i] & 0xFF);
+        }
+        return value;
+    }
+
+    private static double calculateImpliedVolatility(double underlyingPrice, double strikePrice, double riskFreeRate,
+            double timeToMaturity) {
+        // Implement the Black-Scholes formula here
+        // Use the received market data to calculate the implied volatility (IV)
+        // Return the calculated IV
+
+        // You can use the provided fields or other relevant inputs to calculate implied volatility
+
+        // Dummy calculation (replace with actual Black-Scholes formula)
+        return 0.0;
+    }
+}
