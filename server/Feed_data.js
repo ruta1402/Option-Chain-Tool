fd05016f1015f3da52ec7d165abf9206e5ceda6a
ruta1402 <72610095+ruta1402@users.noreply.github.com>
2023-07-04 10:31:33 +0000

    timeToExpiry

--- server/Feed_data.js
+++ server/Feed_data.js
@@ -15,11 +15,11 @@ var idd = 0
 
 
 function processMarketDataPacket(packetData, packetSize) {
+
     // Extract relevant fields from the packet
     const tradingSymbol = packetData.toString('utf8', 4, 34).replaceAll('\x00', '');
     const sequenceNumber = getLongFromLittleEndian(packetData, 34);
-    const timeStamp = getLongFromLittleEndian(packetData, 42);
-    //const epochTime = new Date(timeStamp * 1000);
+    const timeStamp = getLongFromLittleEndian(packetData, 42);  //this is already epoch time
     const lastTradedPrice = getLongFromLittleEndian(packetData, 50) / 100;
     const lastTradedQuantity = getLongFromLittleEndian(packetData, 58);
     const totalTradedVolume = getLongFromLittleEndian(packetData, 66);
@@ -30,9 +30,10 @@ function processMarketDataPacket(packetData, packetSize) {
     const openInterest = getLongFromLittleEndian(packetData, 106);
     const prevClosePrice = getLongFromLittleEndian(packetData, 114) / 100;
     const prevOpenInterest = getLongFromLittleEndian(packetData, 122);
+    //End of extract relevant fields from the packet
 
 
-    //Index
+    //Extract Index
     var a = "ALLBANKS";
     var b = "MAINIDX";
     var c = "FINANCIALS";
@@ -56,11 +57,10 @@ function processMarketDataPacket(packetData, packetSize) {
         index = d;
         id = idd++
     }
+    //End of extract index
 
-    //to check why array out of bound exception
-
-    //expiry date
-
+    
+    //Extract expiry date
     var expiryDate = tradingSymbol.substring(index.length, index.length + 7);
 
     var month = expiryDate.substring(2, 5);
@@ -97,35 +97,18 @@ function processMarketDataPacket(packetData, packetSize) {
     } else if (expiryDate.endsWith("24")) {
         targetYear = 2024;
     }
+    //End of extract expiry date
 
-    // Set the target month and year
-    // Month (0-11) - 0 represents January, 1 represents February, and so on
-    // Year (e.g., 2023)
-
-    // Create a new Date object for the first day of the next month
-    const firstDayOfNextMonth = new Date(targetYear, targetMonth + 1, 1);
-
-    // Set the date to the last day of the current month
-    const lastDayOfMonth = new Date(firstDayOfNextMonth - 1);
-
-    // Get the current day of the week (0 = Sunday, 1 = Monday, ..., 6 = Saturday)
-    const currentDayOfWeek = lastDayOfMonth.getDay();
-
-    // Calculate the difference in days to the last Thursday
-    const differenceInDays = (currentDayOfWeek + 6) % 7;
-
-    // Calculate the epoch time for the last Thursday of the month
-    const epochTimeThurs1530 = Math.floor(lastDayOfMonth.getTime() - differenceInDays * 24 * 60 * 60 * 1000) / 1000;
-
-    // Output the result
-    console.log('Epoch Time of last thurs:', epochTimeThurs1530);
 
 
     //calculate epoch of expiry date
-    const expiry_Date = new Date(targetYear, targetMonth, date);
+    const expiry_Date = new Date(targetYear, targetMonth, date,15,30); 
+    const epochTimeExpiry = Math.floor(expiry_Date.getTime());
+    //End of epoch expiry 
+    
 
-    // Convert the epoch time to seconds by dividing it by 1000
-    const epochTimeExpiry = Math.floor(expiry_Date.getTime() / 1000);
+    //time to maturity
+    var timeToMaturity = epochTimeExpiry-timeStamp;
 
 
     //strike price
@@ -134,46 +117,31 @@ function processMarketDataPacket(packetData, packetSize) {
     var options = tradingSymbol.slice(index.length + 7, tradingSymbol.length).replace(/[^A-Z]/g, '');
     let option = '';
 
-    // Extract other fields as needed
+    //Extract option type
     if (options == 'CE') {
         option = 'call';
     } else if (options == 'PE') {
         option = 'put';
     }
+
     //change in oi
     var chngInOI = openInterest - prevOpenInterest;
-
-    //time to maturity
-
-    // const now = new Date(); // Current date and time
-    // const targetTime = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 15, 30); // Set time to 15:30
-
-    //const exp1530epoch = Math.floor(targetTime.getTime() / 1000); // Convert to epoch time in seconds
-
-
-    // var timeToMaturity = (exp1530epoch - epochTime)<0?0:exp1530epoch - epochTime;
-    var timeToMaturity = epochTimeThurs1530 - epochTimeExpiry;
-    // if(option!==''){
-
-    //     var implied_volatility = calculate_implied_volatility(bestBid,bestAsk , 0.05,timeToMaturity , lastTradedPrice, option);
-    // }
-    // Do further processing or display the extracted data
-
+   
 
     //implied volatilty
     // Set the inputs
-    // const optionPrice = 10.0; // Replace with the actual option price
-    // const underlyingPrice = 100.0; // Replace with the current underlying asset price
-    // const strikePrice = 95.0; // Replace with the option's strike price
-    // const timeToExpiration = 0.5; // Replace with the time to expiration in years
+    const optionPrice = 10.0; // Replace with the actual option price
+    const underlyingPrice = lastTradedPrice; // Replace with the current underlying asset price
+    const strikePrice = strikePriceInt; // Replace with the option's strike price
+    const timeToExpiration = timeToMaturity/31536000000; // Replace with the time to expiration in years
     // const riskFreeRate = ; // Replace with the risk-free interest rate
 
     // Calculate implied volatility
     const impliedVolatility = bs.blackScholes(
-        bestAsk,
-        lastTradedPrice,
-        strikePriceInt,
-        timeToMaturity,
+        optionPrice,
+        underlyingPrice,
+        strikePrice,
+        timeToExpiration,
         0.05,
         option
     );
@@ -206,8 +174,6 @@ function processMarketDataPacket(packetData, packetSize) {
         // console.log(data);
     return data
 
-
-    // Implement the rest of the processing logic as needed
 }
 
 function getLongFromLittleEndian(data, offset) {
